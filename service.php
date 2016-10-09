92a4534d4ee191aed4e1c7409e5d3ebe2863d702
salvipascual <salvi.pascual@gmail.com>
2016-10-08 21:30:09 -0400

    fix service

--- service.php
+++ service.php
@@ -2,102 +2,88 @@
 
 use Goutte\Client;
 
-class Letra extends Service {
-	
+class Letra extends Service
+{
 	static $results = array();
-	
+
 	/**
 	 * Function executed when the service is called
 	 *
+	 * @author salvipascual
 	 * @param Request
 	 * @return Response
 	 * */
-	public function _main(Request $request){
-		
+	public function _main(Request $request)
+	{
 		$argument = trim($request->query);
-		
-		if ($argument=='') {			
+
+		if ($argument=='')
+		{
 			$response = new Response();
 			$response->setResponseSubject("Falta el nombre de la cancion");
 			$response->createFromText("No escribi&oacute; el nombre de la canci&oacute;n", array());
 			return $response;
 		}
-		
+
+		// get the best matching lyric
 		$lyric = $this->getLyric($argument);
-		
-		if ($lyric === false){
+
+		if ($lyric === false)
+		{
 			$response = new Response();
 			$response->setResponseSubject("Letra de cancion no encontrada");
 			$response->createFromText("Letra de canci&oacute;n no encontrada para <b>$argument</b>. Verfica que escribiste bien el nombre de la canci&oacute;n. Si el problema persiste contacta con el soporte t&eacute;cnico.", array());
 			return $response;
 		}
-		
+
 		// create response
-		$responseContent = array (
-				"title" => $lyric['title'],
-				"author" => $lyric['author'],
-				"lyric" => $lyric['body'],
-				"song" => ucfirst($argument)
+		$responseContent = array(
+			"title" => $lyric['title'],
+			"author" => $lyric['author'],
+			"lyric" => $lyric['body'],
+			"song" => ucfirst($argument)
 		);
-	
+
 		$response = new Response();
 		$response->setResponseSubject("Letra: ".$lyric['title']."");
 		$response->createFromTemplate("basic.tpl", $responseContent);
-		
+
 		return $response;
 	}
-	
-	/**
-	  * Search lyric on the web http://lyrics.com
-	  */
-	public function getLyric($search){
-		
-		$client = new Client();
-		
-		$url = 'http://www.lyrics.com/search.php?keyword='.$search.'&what=all&search_btn=Search';
-		//$url = 'http://localhost/test/search.htm';
-		
-		$crawler = $client->request('GET', $url);
-		
-		// <a href="/yesterday-lyrics-the-beatles.html" class="lyrics_preview" t_id="T  4592984">Yesterday</a>
-		// <div id="lyrics" class="SCREENONLY" itemprop="description">Yesterday, all my troubles seemed so far away,<br>
 
-		self::$results = array();
-		
-		try {
-			$crawler->filter('a.lyrics_preview')->each(function ($node, $i) {
-				 self::$results[] = $node->attr('href');
-			});
-		} catch(Exception $e){
+	/**
+	 * Search lyric on the web
+	 *
+	 * @author salvipascual
+	 */
+	public function getLyric($search)
+	{
+		try
+		{
+			// access the list of possible songs
+			$client = new Client();
+			$crawler = $client->request('GET', 'http://www.lyricsfreak.com/search.php?a=search&type=song&q='.$search);
+
+			// acccess the first page on the list of songs
+			$link = $crawler->filter('a.song')->first()->attr('href');
+			$lyricsPage = $client->request('GET', "http://www.lyricsfreak.com".$link);
+
+			// get the info from the songs page
+			$titleANDauthor = $lyricsPage->filter('.lyric-song-head');
+			$title = trim(explode("–", $titleANDauthor->html())[1], "Lyrics ");
+			$author = $titleANDauthor->filter('a')->html();
+			$body = $lyricsPage->filter('#content_h')->html();
+		}
+		catch(Exception $e)
+		{
 			return false;
 		}
-		
-		foreach(self::$results as $result){
-			
-			try {
-				$url = "http://".str_replace("//","/","www.lyrics.com/".$result);
-				$crawler = $client->request('GET', $url);
-				
-				$title = $crawler->filter('#profile_name')->html();
-				$title = substr($title,0,strpos($title,'<br'));
-				$author = $crawler->filter('#profile_name > span > a')->text();
-				$body = $crawler->filter('#lyrics')->html();
-				$pos = strpos($body,"---");
-				
-				if ($pos !== false) 
-					$body = substr($body, 0, $pos);
-				
-				return array(
-					'title' => $title,
-					'body' => $body,
-					'author' => $author
-				);
-			} catch(Exception $e) {
-				// continue
-			}
-			
-		}
-		return false;
+
+		return array(
+			'title' => $title,
+			'body' => $body,
+			'author' => $author
+		);
 	}
-	
+
 }
--- templates/basic.tpl
+++ templates/basic.tpl
@@ -1,4 +1,13 @@
-<h1>{$title}</h1>
-por <b>{$author}</b><br/>
-{button href="PARTITURA {$title}" caption="Ver parituras"}<br/>
+<table border="0" cellspading="0" cellspacing="0" width="100%">
+<tr>
+	<td>
+		<h1>{$title}</h1>
+		por <b>{$author}</b><br/>
+	</td>
+	<td align="right">
+		{button href="PARTITURA {$title}" caption="Ver parituras"}<br/>
+	</td>
+<tr>
+</table>
+
 <p>{$lyric}</p>
